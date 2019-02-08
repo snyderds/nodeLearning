4bd2d0ed8ed53652206ea9eda1445698af39138a
David Snyder <cloudaful@gmail.com>
2019-02-07 22:20:57 -0500

    Added filesystem usage

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
 function rqListener(req, res) {
-    console.log(req.url, req.method, req.headers);
+    //console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+
+
+    if (url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+
+    }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
